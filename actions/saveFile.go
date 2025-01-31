4eac63b62683bf291118d3650fb52eef34a70389
bloodofspring <t.shagin@gmail.com>
2025-01-31 21:28:52 +0300

    feat: added code to save photos in hq

--- actions/saveFile.go
+++ actions/saveFile.go
@@ -1,6 +1,7 @@
 package actions
 
 import (
+	"bytes"
 	"fmt"
 	"io"
 	"log"
@@ -17,54 +18,67 @@ type SaveFile struct {
 }
 
 func (e SaveFile) FabricateAnswer(update tgbotapi.Update) tgbotapi.Chattable {
-	// msg := tgbotapi.NewPhoto(update.BusinnesMessage.From.ID, tgbotapi.FileID(update.BusinnesMessage.ReplyToMessage.Photo[0].FileID))
-	// msg.Caption = "Фото собеседника"
-	msg := tgbotapi.NewMessage(update.BusinnesMessage.From.ID, "test")
+	msg := tgbotapi.NewMessage(update.BusinnesMessage.From.ID, "Сохранено горячее фото собеседника!")
 
 	return msg
 }
 
 func (e SaveFile) Run(update tgbotapi.Update) error {
-	file, err := e.Client.GetFile(tgbotapi.FileConfig{FileID: update.BusinnesMessage.ReplyToMessage.Photo[0].FileID})
-    if err != nil {
-        log.Fatal("Something went wrong with saving user file!")
-    }
-
-	fmt.Println(file.Link(e.Client.Token))
-
-	testLink := fmt.Sprintf("https://api.telegram.org/bot%s/getFile?file_id=%s", e.Client.Token, update.BusinnesMessage.ReplyToMessage.Photo[0].FileID)
-	fmt.Println(testLink)
-
-    resp, err := http.Get(file.Link(e.Client.Token))
-    if err != nil {
-        log.Fatal("Something went wrong with accessing user file url!")
-    }
-
-    defer resp.Body.Close()
-
-    // if _, err := os.Stat("/downloads"); err == nil {
-    //  // path/to/whatever exists
-    //   } else if errors.Is(err, os.ErrNotExist) {
-    //  os.Mkdir("/downloads", os.FileMode{}})
-    //   }
-
-    out, err := os.Create("downloads/test.jpg")
-    if err != nil {
-        log.Fatal(err)
-    }
-    
-    defer out.Close()
-
-    _, err = io.Copy(out, resp.Body)
-    if err != nil {
-        log.Fatal(err)
-    }
-
-	if _, err := e.Client.Send(e.FabricateAnswer(update)); err != nil {
-		return err
+	fileID := update.BusinnesMessage.ReplyToMessage.Photo[len(update.BusinnesMessage.ReplyToMessage.Photo)-1].FileID
+	
+	file, err := e.Client.GetFile(tgbotapi.FileConfig{FileID: fileID})
+	if err != nil {
+		return fmt.Errorf("не удалось получить информацию о файле: %v", err)
 	}
 
-	return nil
+	fileURL := file.Link(e.Client.Token)
+
+	resp, err := http.Get(fileURL)
+	if err != nil {
+		return fmt.Errorf("ошибка при скачивании файла: %v", err)
+	}
+	defer resp.Body.Close()
+
+	// Создаем файл для сохранения
+	outputFile, err := os.Create(fmt.Sprintf("downloads/%s.jpg", fileID))
+	if err != nil {
+		return fmt.Errorf("ошибка при создании файла: %v", err)
+	}
+	defer outputFile.Close()
+
+	// Буфер для чтения по частям
+	buffer := make([]byte, 32*1024) // 32KB chunks
+	
+	// Создаем буфер в памяти для накопления данных
+	var buf bytes.Buffer
+	
+	// Читаем файл частями
+	for {
+		n, err := resp.Body.Read(buffer)
+		if n > 0 {
+			// Записываем часть в буфер
+			buf.Write(buffer[:n])
+		}
+		
+		if err == io.EOF {
+			break
+		}
+		
+		if err != nil {
+			return fmt.Errorf("ошибка при чтении файла: %v", err)
+		}
+	}
+
+	// Записываем весь буфер в файл
+	_, err = buf.WriteTo(outputFile)
+	if err != nil {
+		return fmt.Errorf("ошибка при записи в файл: %v", err)
+	}
+
+	log.Printf("Файл успешно сохранен: %s.jpg", fileID)
+	_, err = e.Client.Send(e.FabricateAnswer(update))
+
+	return err
 }
 
 func (e SaveFile) GetName() string {
