1d95730f8542349ef91419eb403c605ec421c0c5
bloodofspring <t.shagin@gmail.com>
2025-02-01 21:02:34 +0300

    feat: added file saveVideoMessage.go for saving telegram video notes

---
+++ actions/saveVideoMessage.go
@@ -0,0 +1,92 @@
+package actions
+
+import (
+	"fmt"
+	"io"
+	"log"
+	"net/http"
+	"os"
+	"path/filepath"
+
+	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api/v5"
+)
+
+type saveVideoMessageCallback struct {
+	Name string
+	Client tgbotapi.BotAPI
+}
+
+
+func (e saveVideoMessageCallback) fabricateAnswer(update tgbotapi.Update, fileID string) tgbotapi.Chattable {
+    filePath := fmt.Sprintf("downloaded_videos/%s.jpg", fileID)
+    photoBytes, err := os.ReadFile(filePath)
+    if err != nil {
+        log.Printf("Ошибка при чтении файла: %v", err)
+        return tgbotapi.NewMessage(update.BusinnesMessage.From.ID, "Не удалось отправить файл")
+    }
+
+    videoNoteFile := tgbotapi.FileBytes{
+        Name:  "videoNote.jpg",
+        Bytes: photoBytes,
+    }
+
+    videoNoteMsg := tgbotapi.NewPhoto(update.BusinnesMessage.From.ID, videoNoteFile)
+
+	defer func() {
+		if err := os.Remove(filePath); err != nil {
+			log.Printf("Ошибка при удалении файла %s: %v", filePath, err)
+		}
+	}()
+
+	return videoNoteMsg
+}
+
+
+func (e *saveVideoMessageCallback) Run(update tgbotapi.Update) error {
+	// Формируем URL для загрузки файла
+	fileID := update.BusinnesMessage.VideoNote.FileID
+	file, err := e.Client.GetFile(tgbotapi.FileConfig{FileID: fileID})
+	if err != nil {
+		return fmt.Errorf("не удалось получить информацию о файле: %v", err)
+	}
+
+	fileURL := file.Link(e.Client.Token)
+
+	// Создаем HTTP клиент
+	client := &http.Client{}
+	resp, err := client.Get(fileURL)
+	if err != nil {
+		return fmt.Errorf("ошибка при получении файла: %w", err)
+	}
+	defer resp.Body.Close()
+
+	// Создаем директорию для сохранения, если её нет
+	saveDir := "downloaded_videos"
+	if err := os.MkdirAll(saveDir, 0755); err != nil {
+		return fmt.Errorf("ошибка при создании директории: %w", err)
+	}
+
+	// Создаем файл для сохранения
+	filePath := filepath.Join(saveDir, fileID+".mp4")
+	out, err := os.Create(filePath)
+	if err != nil {
+		return fmt.Errorf("ошибка при создании файла: %w", err)
+	}
+	defer out.Close()
+
+	// Используем буферизированное копирование для больших файлов
+	buf := make([]byte, 1024*1024) // 1MB буфер
+	_, err = io.CopyBuffer(out, resp.Body, buf)
+	if err != nil {
+		return fmt.Errorf("ошибка при сохранении файла: %w", err)
+	}
+
+	_, err = e.Client.Send(e.fabricateAnswer(update, fileID))
+
+	return err
+}
+
+
+func (e *saveVideoMessageCallback) GetName() string {
+	return e.Name
+}
