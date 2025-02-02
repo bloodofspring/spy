d1ca81458f94ce352c4f334dab46a0e7bfa82f86
bloodofspring <t.shagin@gmail.com>
2025-02-02 21:50:46 +0300

    feat: refactored saveAudioMessage.go

--- actions/saveAudioMessage.go
+++ actions/saveAudioMessage.go
@@ -17,12 +17,11 @@ type SaveVoiceMessageCallback struct {
 	Client tgbotapi.BotAPI
 }
 
-func (e SaveVoiceMessageCallback) fabricateAnswer(update tgbotapi.Update, fileID string) tgbotapi.Chattable {
+func (e SaveVoiceMessageCallback) fabricateAnswer(update tgbotapi.Update, fileID string) (tgbotapi.Chattable, error) {
 	filePath := fmt.Sprintf("downloaded_voice/%s.mp3", fileID)
 	voiceBytes, err := os.ReadFile(filePath)
 	if err != nil {
-		log.Printf("Ошибка при чтении файла: %v", err)
-		return tgbotapi.NewMessage(update.BusinnesMessage.From.ID, "Не удалось отправить файл")
+		return tgbotapi.NewMessage(-1, ""), fmt.Errorf("ошибка при чтении файла: %v", err)
 	}
 
 	voiceNoteFile := tgbotapi.FileBytes{
@@ -38,7 +37,7 @@ func (e SaveVoiceMessageCallback) fabricateAnswer(update tgbotapi.Update, fileID
 		}
 	}()
 
-	return voiceMsg
+	return voiceMsg, nil
 }
 
 func (e SaveVoiceMessageCallback) Run(update tgbotapi.Update) error {
@@ -54,16 +53,13 @@ func (e SaveVoiceMessageCallback) Run(update tgbotapi.Update) error {
 
 	fileURL := file.Link(e.Client.Token)
 
-	// Создаем директорию для сохранения, если её нет
 	voiceDir := "downloaded_voice"
 	if err := os.MkdirAll(voiceDir, 0755); err != nil {
 		return fmt.Errorf("ошибка создания директории: %w", err)
 	}
 
-	// Формируем путь для сохранения файла
 	filePath := filepath.Join(voiceDir, fmt.Sprintf("%s.mp3", fileID))
 
-	// Скачиваем файл частями
 	client := &http.Client{}
 	resp, err := client.Get(fileURL)
 	if err != nil {
@@ -71,22 +67,31 @@ func (e SaveVoiceMessageCallback) Run(update tgbotapi.Update) error {
 	}
 	defer resp.Body.Close()
 
-	// Создаем файл для записи
 	out, err := os.Create(filePath)
 	if err != nil {
 		return fmt.Errorf("ошибка создания файла: %w", err)
 	}
 	defer out.Close()
 
-	// Копируем данные частями, используя буфер
-	buffer := make([]byte, 32*1024) // 32KB буфер
+	buffer := make([]byte, 32*1024)
 	_, err = io.CopyBuffer(out, resp.Body, buffer)
 	if err != nil {
 		return fmt.Errorf("ошибка сохранения файла: %w", err)
 	}
 
-	// Отправляем сообщение об успешном сохранении
-	_, err = e.Client.Send(e.fabricateAnswer(update, fileID))
+	ans, err := e.fabricateAnswer(update, fileID)
+	if err != nil {
+		return err
+	}
+
+	sentMsg, err := e.Client.Send(ans)
+	if err != nil {
+		return err
+	}
+
+	msg := tgbotapi.NewMessage(update.BusinnesMessage.From.ID, fmt.Sprintf("Самоуничтожающееся сообщение от @%s", update.BusinnesMessage.ReplyToMessage.From.UserName))
+	msg.ReplyToMessageID = sentMsg.MessageID
+	_, err = e.Client.Send(msg)
 
 	return err
 }
