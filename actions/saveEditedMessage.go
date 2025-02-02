8ed8bd17e3df7adc92cc2c3e44f5166fc3d757ee
bloodofspring <t.shagin@gmail.com>
2025-02-02 20:49:10 +0300

    feat: added update of messages in the database, fixed errors

--- actions/saveEditedMessage.go
+++ actions/saveEditedMessage.go
@@ -4,6 +4,8 @@ import (
 	"fmt"
 	"main/database"
 
+	models "main/database/models"
+
 	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api/v5"
 )
 
@@ -12,19 +14,56 @@ type SaveEdiedMessage struct {
 	Client tgbotapi.BotAPI
 }
 
-func (e SaveEdiedMessage) fabricateAnswer(update tgbotapi.Update) tgbotapi.Chattable {
+func (e SaveEdiedMessage) fabricateAnswer(update tgbotapi.Update) (tgbotapi.Chattable, error) {
 	// ToDo: Добавить сравнение было | стало (после реализации истории событий в бд)
-	msg := tgbotapi.NewMessage(1044385209, fmt.Sprintf("Пользователь %s изменил сообщение:\n<blockquote>%s</blockquote>", update.EditedBusinnesMessage.From.UserName, update.EditedBusinnesMessage.Text))
+	db := database.Connect()
+	defer db.Close()
+
+	messageDb := &models.Message{}
+	err := db.Model(messageDb).
+		Where("tg_id = ? AND business_connection_id = ?", update.EditedBusinnesMessage.MessageID, update.EditedBusinnesMessage.BusinessConnectionId).
+		Select()
+
+	if err != nil {
+		return tgbotapi.NewMessage(-1, ""), err
+	}
+
+	oldMessageText := messageDb.Text
+
+	messageDb.Text = update.EditedBusinnesMessage.Text
+	_, err = db.Model(messageDb).WherePK().Update()
+	if err != nil {
+		return tgbotapi.NewMessage(-1, ""), err
+	}
+
+	sendToDb := &models.TelegramUser{}
+	err = db.Model(sendToDb).
+		Where("business_connection_id = ?", update.EditedBusinnesMessage.BusinessConnectionId).
+		Select()
+	if err != nil {
+		return tgbotapi.NewMessage(-1, ""), err
+	}
+
+	msg := tgbotapi.NewMessage(sendToDb.TgId, fmt.Sprintf("Пользователь %s изменил сообщение:\n<blockquote>%s</blockquote>\nна\n<blockquote>%s</blockquote>", update.EditedBusinnesMessage.From.UserName, oldMessageText, update.EditedBusinnesMessage.Text))
 	msg.ParseMode = "HTML"
-	return msg
+	return msg, nil
 }
 
 func (e SaveEdiedMessage) Run(update tgbotapi.Update) error {
 	if err := database.UpdateAllUserData(update.EditedBusinnesMessage.From.ID, update.EditedBusinnesMessage.BusinessConnectionId, false); err != nil {
 		return err
 	}
+	
+	if update.EditedBusinnesMessage.From.ID != update.EditedBusinnesMessage.Chat.ID {
+		return nil  // Сообщение изменено НЕ собеседником
+	}
 
-	_, err := e.Client.Send(e.fabricateAnswer(update))
+	ans, err := e.fabricateAnswer(update)
+	if err != nil {
+		return err
+	}
+
+	_, err = e.Client.Send(ans)
 
 	return err
 }
