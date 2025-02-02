8264239bc4b8e9b263564c996e1ce5b6332ef654
bloodofspring <t.shagin@gmail.com>
2025-02-02 18:06:26 +0300

    fix: fixed small error with database

--- database/models/messages.go
+++ database/models/messages.go
@@ -6,7 +6,7 @@ type Message struct {
 	TgId       int `pg:",pk"`
 	ChatId     int64
 	FromUserId int64
-	From       *TelegramUser `pg:"rel:has-one"`
+	FromUser   *TelegramUser `pg:"rel:has-one"`
 }
 
 func (m Message) String() string {
