1380882b898e1532eebdd104754b4a2313ec14dc
bloodofspring <t.shagin@gmail.com>
2025-02-04 20:26:00 +0300

    hot-fix: changed db name to correct

--- database/init.go
+++ database/init.go
@@ -14,7 +14,7 @@ func Connect() *pg.DB {
 		Addr:     "localhost:5432",
 		User:     "postgres",
 		Password: envFile["db_password"],
-		Database: "testDb_2",
+		Database: "bigBrotherBotDb",
 	})
 
 	return db
